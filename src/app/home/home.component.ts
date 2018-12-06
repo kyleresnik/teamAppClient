ebed089369b758dc6cc4c55eed3d298fecae3654
Kyle Resnik <kylebresnik@gmail.com>
2018-12-06 15:19:34 -0500

    basic homepage created

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html', 
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
