40c7853aee2553576082e1bcccc9dcd3c0b15cb5
unknown <cbjones0703@gmail.com>
2018-12-12 16:20:07 -0500

    Footer/Profile/Admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms'
-import {MatDialogModule} from '@angular/material';
+// import {MatDialogModule} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
-
-
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
-
 import { DealComponent } from './deal/deal.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AdminComponent } from './admin/admin.component';
 import { ProfileComponent } from './profile/profile.component';
 import { FooterComponent } from './footer/footer.component';
-
 // import { FlexLayoutModule } from '@angular/flex-layout';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-
+import { MatFormFieldModule, MatOptionModule, MatSelectModule, MatFormFieldControl } from '@angular/material';
+import { DemoMaterialModule } from './material-module'
 
 @NgModule({
   declarations: [
@@ -33,11 +29,9 @@ import { LoginComponent } from './login/login.component';
     ProfileComponent,
     FooterComponent,
     AboutComponent,
-    NavComponent,
     RegisterComponent,
     LoginComponent,
-    HomeComponent,
-    NavComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -46,14 +40,15 @@ import { LoginComponent } from './login/login.component';
     HttpClientModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    
-
+    MatFormFieldModule,
+    MatOptionModule,
     // FlexLayoutModule,    
-    ReactiveFormsModule,
-    FormsModule,
-    MatDialogModule,
-    BrowserAnimationsModule,
-    CarouselModule.forRoot()
+    // MatDialogModule,
+    CarouselModule.forRoot(),
+    MatSelectModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    DemoMaterialModule
 
   ],
   providers: [],
